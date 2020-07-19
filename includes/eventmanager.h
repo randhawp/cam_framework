f4c9ad8d43eec2213da11d2501823ba0869bed6b
puneet randhawa <randhawa.puneet@gmail.com>
2020-07-19 21:55:39 +0000

    Changed OnEventReceived to be pure in base class

--- includes/eventmanager.h
+++ includes/eventmanager.h
@@ -8,13 +8,14 @@ class EventManager{
   Event event_;
 public:
 
-  void Publish(const string& message) {
+  virtual void OnEventReceived(void* pSender, const EventArgs& args)=0;
+  void Publish(const string& message)  {
 		event_(this, StringEventArgs(message));
 	}
-	long Register(function<void(void*, const EventArgs&)> f) {
+	long Register(function<void(void*, const EventArgs&)> f)  {
 		return event_.Subscribe(f);
 	}
-	void Unregister(long token) {
+	void Unregister(long token)  {
 		event_.Unsubscribe(token);
 	}
   
